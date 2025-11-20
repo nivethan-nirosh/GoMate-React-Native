607e00b5b7d3399a8bb303f355ad200596a2a9cb
nivethan-nirosh <nirosh.26d@gmail.com>
2025-11-20 22:23:02 +0530

    Integration: Connected Redux Provider and Navigation to App entry point

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/redux/store';
+import AppNavigator from './src/navigation/AppNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
+    </Provider>
+  );
+}
